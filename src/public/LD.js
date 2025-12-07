a1bd41b830548733900400090ffb43d46941f10b
CodeBrush-dev <info@codebrush.dev>
2025-12-07 10:38:03 +0100

    Update LD.js

---
+++ src/public/LD.js
@@ -0,0 +1,50 @@
+export const LD_DATA = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Luc Peiffer",
+    "url": "https://www.lucpeiffer.com/",
+    "image": "https://static.wixstatic.com/media/35660e_dc6b9053e4c5406bb1fdbf4071ea9bbd.jpg",
+    "description": "Né à Bruxelles en 1965, sculpteur et auteur de bande dessinée. Travaille comme dessinateur et sculpteur; plusieurs sculptures exposées en Belgique et aux Pays-Bas. Auteur de \"Un Amour Suspendu\" (2023) avec Pilar Pujadas.",
+    "birthDate": "1965",
+    "birthPlace": {
+        "@type": "Place",
+        "name": "Bruxelles"
+    },
+    "jobTitle": "Sculpteur; auteur de bande dessinée; designer",
+    "affiliation": {
+        "@type": "Organization",
+        "name": "L&A Peiffer Communication & Design"
+    },
+    "works": [
+        {
+            "@type": "Book",
+            "name": "Un Amour Suspendu",
+            "author": [
+                {
+                    "@type": "Person",
+                    "name": "Luc Peiffer"
+                },
+                {
+                    "@type": "Person",
+                    "name": "Pilar Pujadas"
+                }
+            ],
+            "datePublished": "2023",
+            "inLanguage": "fr",
+            "url": "https://kenneseditions.com/product/un-amour-suspendu/"
+        }
+    ],
+    "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": "https://www.lucpeiffer.com/"
+    },
+    "sameAs": [
+        "http://www.galerie-amsterdam.nl/",
+        "http://lifeofl.com",
+        "http://www.smelik-stokking.nl/",
+        "http://www.steyls-art.com/",
+        "https://kenneseditions.com/product/un-amour-suspendu/",
+        "http://www.annmicheledeceuninck.com/",
+        "http://www.henripfr.com"
+    ]
+}
