fa2cf58705acc110641d0cf293da18bc4cb7fc17
CodeBrush-dev <info@codebrush.dev>
2025-12-07 10:38:01 +0100

    Update meta-tags.js

---
+++ src/public/meta-tags.js
@@ -0,0 +1,62 @@
+export const META_DATA = {
+    "meta_tags_list": [
+        {
+            "page_url": "https://www.lucpeiffer.com/",
+            "title_tag": "Bande dessinée & Sculptures Bruxelles | Luc Peiffer",
+            "meta_description": "Artiste Luc Peiffer - sculptures et bande dessinée à Bruxelles. Portfolio, expositions et création artistique en Belgique. L&A Peiffer Communication & design."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/bruxelles-se-ressource",
+            "title_tag": "Bruxelles se ressource - Nageurs en bronze | Luc Peiffer",
+            "meta_description": "Série de petits nageurs abandonnés aux fontaines de Bruxelles. Nageurs en bronze, mémoire urbaine et expositions par Luc Peiffer."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/splash",
+            "title_tag": "A Little Splash - Nageurs en bronze | Luc Peiffer",
+            "meta_description": "A Little Splash : ensemble de petites nageuses, création artistique et installation exposée à Bruxelles. Œuvres de Luc Peiffer."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/fontaine",
+            "title_tag": "Fontaine - Nageurs en bronze | Luc Peiffer",
+            "meta_description": "Fontaine : sculpture en bronze (175x55 cm) exposée à Bruxelles. Œuvre de Luc Peiffer mêlant arts plastiques et installation publique."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/bd",
+            "title_tag": "Un Amour Suspendu - Bande dessinée Bruxelles | Luc Peiffer",
+            "meta_description": "Un Amour Suspendu (2023) - bande dessinée de Luc Peiffer et Pilar Pujadas. Roman graphique publié en Belgique. Découvrez la BD et le portfolio."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/etudes",
+            "title_tag": "Portfolio Luc Peiffer - Etudes & Maquettes | Luc Peiffer",
+            "meta_description": "Etudes, maquettes et recherches de Luc Peiffer. Portfolio d'arts plastiques, projets en cours et créations artistiques en Belgique et Bruxelles."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/babies",
+            "title_tag": "Babies - Arts plastiques Bruxelles | Luc Peiffer",
+            "meta_description": "Babies : série colorée de sculptures explorant l'enfance et la consommation. Expositions à Bruxelles et œuvres de l'artiste Luc Peiffer."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/greco",
+            "title_tag": "Hommage au Greco - Sculptures Bruxelles | Luc Peiffer",
+            "meta_description": "Hommage au Greco : ensemble de 12 personnages inspirés d'El Greco. Sculptures et expositions à Bruxelles par l'artiste Luc Peiffer."
+        },
+        {
+            "page_url": "https://www.lucpeiffer.com/globes",
+            "title_tag": "Alte Kopfstücke - Arts plastiques Bruxelles | Luc Peiffer",
+            "meta_description": "Alte Kopfstücke : têtes compressées représentant isolement et vieillesse. Sculptures exposées à Bruxelles par Luc Peiffer."
+        }
+    ],
+    "keywords": [
+        "bande dessinee bruxelles",
+        "sculptures bruxelles",
+        "artiste luc peiffer",
+        "l&a peiffer communication",
+        "expositions bruxelles",
+        "nageurs en bronze",
+        "arts plastiques bruxelles",
+        "un amour suspendu",
+        "creation artistique belgique",
+        "portfolio luc peiffer",
+        "henri pfr"
+    ]
+}
